8a69ac1b7f5cf67696c1cca96907b4586d28f5b6
Syris Chan <chung.chan.18@ucl.ac.uk>
2019-10-17 11:48:53 +0100

    making a dictionary as asked

--- group.py
+++ group.py
@@ -1,5 +1,21 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+# 'name','age', 'job', 'connection'
 
-my_group =
+my_group = [{"name":"Jill","age":"26","job":"biologist","connections":["Zalikas's friend", "John's partner"]},
+             {"name":"Zalika","age":"28","job":"artist","connections":["Jill's friend"]},
+                {"name":"John","age":"27","job":"writer","connections":["Jill's partner"]},
+                    {"name":"Nash","age":"34","job":"chef","connections":["John's cousin", "Zalika's landlord"]}]
+#  including name, age, job, connection inside the dict
+#connections = [Partner={},
+#                    Friend={},
+ #                       Landlord={},
+ #                           cousin={}]
+#attempt to make a dict of connections
+
+
+#1) It allows if they specifies that he has no job in the specific section
+
+#2) similar to Q1
+
+#3) no it does not
